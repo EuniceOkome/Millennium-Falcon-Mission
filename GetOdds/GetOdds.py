f87a9f55aadc4295a4b76663d6fdad2b65be0598
Eunice Okome Obiang <71518626+EuniceOkome@users.noreply.github.com>
2022-11-14 17:48:59 +0100

    Add files via upload

--- GetOdds/GetOdds.py
+++ GetOdds/GetOdds.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  8 15:55:33 2022
-
-@author: euniceokomeobiang
-"""
-
 import numpy as np
 import json
 import sqlite3
--- GetOdds/__init__.py
+++ GetOdds/__init__.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 13 09:29:28 2022
-
-@author: euniceokomeobiang
-"""
-
 from .GetOdds import calcul
 
 
